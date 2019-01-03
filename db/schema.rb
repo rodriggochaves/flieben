7c5943d7205242e3ad05ee498d49339af0eda5c3
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-01-02 22:28:28 -0200

    Task#completed is default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_131656) do
+ActiveRecord::Schema.define(version: 2019_01_03_002658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.text "description"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
